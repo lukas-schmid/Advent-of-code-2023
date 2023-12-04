cbf7d301c13e4a61bdc0cb2f0147af217310a9a7
lukas-schmid <schmid.luk@hotmail.com>
2023-12-04 22:37:18 +0100

    solution 2023-12-04 part1

---
+++ 2023-12-04/main.cpp
@@ -0,0 +1,8 @@
+#include "part1.h"
+#include <iostream>
+
+int main()
+{
+    resultPart1();
+    return 0;
+}
---
+++ 2023-12-04/part2.cpp
@@ -0,0 +1,15 @@
+/*
+    Task (Part2):
+
+    ...
+
+    Result (Part2): xxx
+*/
+
+#include "part2.h"
+#include <iostream>
+
+void resultPart2()
+{
+    std::cout << "hello" << std::endl;
+}
